cf2942fc130e03eb542d6fa55cd61d5e153602d6
mdsxxd5 <mdsaad5586@gmail.com>
2025-11-20 15:24:06 +0530

    day 4 code

---
+++ day 4/2_variable.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdbool.h>
+int main()
+{
+    int age = 30;
+    float fees = 200.25;
+    char name = 's';
+    double pi = 3.145757;
+    bool iswalking = true;
+
+printf("your age id %d \n",age);
+printf("fess is %f \n",fees);
+printf("name is %c \n",name);
+printf("pi is %lf \n",pi);
+printf("is walking %d",iswalking);
+return 0;
+
+}
